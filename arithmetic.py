3f922377d18ed82041523d4793be3b83067c4872
Michelle Chen <mich.chen.94@gmail.com>
2020-07-08 12:43:43 -0700

    power()

--- arithmetic.py
+++ arithmetic.py
@@ -52,6 +52,10 @@ def cube(num1):
 def power(num1, num2):
     """Raise num1 to the power of num2 and return the value."""
 
+    powered = num1**num2
+
+    return num1**num2
+
 
 def mod(num1, num2):
     """Return the remainder of num1 / num2."""
