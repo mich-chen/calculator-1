3712566919afc5662f710850605e4696b5d958a3
Michelle Chen <mich.chen.94@gmail.com>
2020-07-08 12:46:26 -0700

    cleaned up functions

--- arithmetic.py
+++ arithmetic.py
@@ -4,49 +4,37 @@
 def add(num1, num2):
     """Return the sum of the two inputs."""
 
-    add_nums = num1 + num2
-
-    return add_nums
+    return num1 + num2
 
 
 def subtract(num1, num2):
     """Return the second number subtracted from the first."""
 
-    subtracted = num1 - num2
-
-    return subtracted
+    return num1 - num2
 
 
 def multiply(num1, num2):
     """Multiply the two inputs together."""
 
-    multiplied = num1 * num2
-
-    return multiplied
+    return num1 * num2
 
 
 def divide(num1, num2):
     """Divide the first input by the second and return the result."""
 
-    divided = num1/num2
-
-    return divided
+    return num1/num2
 
 
 def square(num1):
     """Return the square of the input."""
 
-    squared = num1**2
-
-    return squared
+    return num1**2
 
 
 def cube(num1):
     """Return the cube of the input."""
 
-    cubed = num1**3
-
-    return cubed
+    return num1**3
 
 
 def power(num1, num2):
